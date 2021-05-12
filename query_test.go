c99113c96508c8faee9e95dac68c53edb8d4ab61
zeddo123 <m.drissi@protonmail.com>
2021-05-12 03:44:52 +0100

    Add unittest for QueryUrl

---
+++ query_test.go
@@ -0,0 +1,31 @@
+package gol_test
+
+import (
+	"testing"
+
+	"github.com/Open-pi/gol"
+)
+
+//func TestQuery(t *testing.T) {
+	//t.Parallel()
+
+//}
+
+func TestQueryURL(t *testing.T) {
+	tt := []struct {
+		name string
+		expected string
+		output string
+	}{
+		{"query editions by author with limit", gol.QueryUrl().Type("edition").Author("OL236174A").Limit(2).Construct(), "https://openlibrary.org/query.json?&type=/type/edition&authors=/authors/OL236174A&limit=2"},
+		{"query editions by author and get title", gol.QueryUrl().Type("edition").Author("OL236174A").Title("").Construct(), "https://openlibrary.org/query.json?&type=/type/edition&authors=/authors/OL236174A&title="},
+	}
+	
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.expected != tc.output {
+				t.Fatalf("got unexpected result")
+			}
+		})
+	}
+}
