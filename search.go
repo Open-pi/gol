67db6ebfdbde7411c11e958639c0fc87d61b15bf
zeddo123 <m.drissi@protonmail.com>
2021-11-25 16:44:56 +0100

    Refactor search

--- search.go
+++ search.go
@@ -1,89 +1,9 @@
 package gol
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"strings"
 )
-
-// SearchData holds all information about Search coming from openlibrary
-type SearchData struct {
-	NumFound int         `json:"num_found"`
-	Start    int         `json:"start"`
-	Docs     []SearchDoc `json:"docs"`
-	Error    string      `json:"error"`
-}
-
-// SearchDoc holds all information about Search Docs
-type SearchDoc struct {
-	Key                               string   `json:"key"`
-	Type                              string   `json:"type"`
-	Title                             string   `json:"title"`
-	TitleSuggest                      string   `json:"title_suggest"`
-	HasFulltext                       bool     `json:"has_fulltext"`
-	EditionCount                      int      `json:"edition_count"`
-	FirstPublishYear                  int      `json:"first_publish_year"`
-	LastModifiedI                     int      `json:"last_modified_i"`
-	EbookCountI                       int      `json:"ebook_count_i"`
-	PublicScanB                       bool     `json:"public_scan_b,omitempty"`
-	IaCollectionS                     string   `json:"ia_collection_s,omitempty"`
-	LendingEditionS                   string   `json:"lending_edition_s,omitempty"`
-	LendingIdentifierS                string   `json:"lending_identifier_s,omitempty"`
-	PrintdisabledS                    string   `json:"printdisabled_s,omitempty"`
-	CoverEditionKey                   string   `json:"cover_edition_key,omitempty"`
-	CoverI                            int      `json:"cover_i,omitempty"`
-	PublishYear                       []int    `json:"publish_year"`
-	AuthorName                        []string `json:"author_name"`
-	IDAmazon                          []string `json:"id_amazon,omitempty"`
-	Seed                              []string `json:"seed"`
-	AuthorAlternativeName             []string `json:"author_alternative_name"`
-	Subject                           []string `json:"subject,omitempty"`
-	Isbn                              []string `json:"isbn,omitempty"`
-	IaLoadedID                        []string `json:"ia_loaded_id,omitempty"`
-	EditionKey                        []string `json:"edition_key"`
-	Language                          []string `json:"language,omitempty"`
-	IDLibrarything                    []string `json:"id_librarything,omitempty"`
-	Lcc                               []string `json:"lcc,omitempty"`
-	Lccn                              []string `json:"lccn,omitempty"`
-	IDGoodreads                       []string `json:"id_goodreads,omitempty"`
-	PublishPlace                      []string `json:"publish_place,omitempty"`
-	Contributor                       []string `json:"contributor,omitempty"`
-	IDGoogle                          []string `json:"id_google,omitempty"`
-	Ia                                []string `json:"ia,omitempty"`
-	Text                              []string `json:"text"`
-	Place                             []string `json:"place,omitempty"`
-	Ddc                               []string `json:"ddc,omitempty"`
-	AuthorKey                         []string `json:"author_key"`
-	IDLibris                          []string `json:"id_libris,omitempty"`
-	IDOverdrive                       []string `json:"id_overdrive,omitempty"`
-	IDDepSitoLegal                    []string `json:"id_depósito_legal,omitempty"`
-	IDDnb                             []string `json:"id_dnb,omitempty"`
-	IDAlibrisID                       []string `json:"id_alibris_id,omitempty"`
-	IaBoxID                           []string `json:"ia_box_id,omitempty"`
-	FirstSentence                     []string `json:"first_sentence,omitempty"`
-	Person                            []string `json:"person,omitempty"`
-	IDWikidata                        []string `json:"id_wikidata,omitempty"`
-	Oclc                              []string `json:"oclc,omitempty"`
-	Publisher                         []string `json:"publisher,omitempty"`
-	IDBcid                            []string `json:"id_bcid,omitempty"`
-	PublishDate                       []string `json:"publish_date"`
-	IDAmazonDeAsin                    []string `json:"id_amazon_de_asin,omitempty"`
-	IDAmazonItAsin                    []string `json:"id_amazon_it_asin,omitempty"`
-	IDNla                             []string `json:"id_nla,omitempty"`
-	IDBritishNationalBibliography     []string `json:"id_british_national_bibliography,omitempty"`
-	IDAmazonCoUkAsin                  []string `json:"id_amazon_co_uk_asin,omitempty"`
-	Time                              []string `json:"time,omitempty"`
-	IDAmazonCaAsin                    []string `json:"id_amazon_ca_asin,omitempty"`
-	IDPaperbackSwap                   []string `json:"id_paperback_swap,omitempty"`
-	IDBibliothQueNationaleDeFranceBnf []string `json:"id_bibliothèque_nationale_de_france_bnf,omitempty"`
-	IDBritishLibrary                  []string `json:"id_british_library,omitempty"`
-	IDScribd                          []string `json:"id_scribd,omitempty"`
-	IDHathiTrust                      []string `json:"id_hathi_trust,omitempty"`
-	IDCanadianNationalLibraryArchive  []string `json:"id_canadian_national_library_archive,omitempty"`
-	Subtitle                          string   `json:"subtitle,omitempty"`
-}
 
 // SearchURL holds information about the Search URL
 type SearchURL struct {
@@ -95,16 +15,16 @@ type SearchURL struct {
 }
 
 // Search returns search API data from a constructed url
-func Search(s string) (sd SearchData, err error) {
-	resp, err := http.Get(s)
+func Search(s string) (Container, error) {
+	container, err := Request(s)
 	if err != nil {
-		return sd, err
+		return nil, err
 	}
 
-	defer resp.Body.Close()
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	json.Unmarshal(bodyBytes, &sd)
-	return
+	if err := HasError(container); err != nil {
+		return nil, err
+	}
+	return container, nil
 }
 
 // Url returns OpenLibrary's search url
--- search_test.go
+++ search_test.go
@@ -1,68 +1,49 @@
 package gol_test
 
-/*
 import (
 	"testing"
 
 	"github.com/Open-pi/gol"
-	"github.com/google/go-cmp/cmp"
 )
 
 func TestSearch(t *testing.T) {
-	t.Parallel()
-	// TODO: Add search by Text, Lists, and Avcanced search
-	tt := []struct {
-		name  string
-		input string
-		tr    gol.SearchData
-	}{
-		{"Test Simple Search", gol.SearchUrl().All("the selfish gene s").Construct(), searchQ},
-		{"Test Title Search", gol.SearchUrl().Title("spellslinger 6").Construct(), searchTitle},
-		{"Test Author Search", gol.SearchUrl().Author("Sarah Penner").Construct(), searchAuthor},
-		{"Test Subject Search", gol.SearchUrl().Subject("abcd").Construct(), searchSubject},
-		{"Test Mixed Search", gol.SearchUrl().Author("Richard Dawkins").Title("The Selfish Gene").Subject("evolution").Construct(), searchMixed}}
-
-	for _, tc := range tt {
-		tc := tc // capture range variable
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			tr, err := gol.Search(tc.input)
-
-			if err != nil {
-				t.Fatalf("got unexpected error: %v", err)
-			}
-
-			if !cmp.Equal(tc.tr, tr) {
-				t.Fatalf("Unexpected result for Search()")
-			}
-		})
-	}
+	//t.Parallel()
+	//tr, err := gol.Search(gol.SearchUrl().Subject("abcd").Construct())
+	//if err != nil {
+	//log.Println(err)
+	//}
+	//log.Println(tr)
 }
 
 func TestSearchURL(t *testing.T) {
 	tr := gol.SearchUrl().All("the selfish gene s").Construct()
+	var searchQURL = "https://openlibrary.org/search.json?&q=the+selfish+gene+s"
+
 	if tr != searchQURL {
 		t.Errorf("Incorrect URL construction of Search query")
 	}
 
+	var searchTitleURL = "https://openlibrary.org/search.json?&title=spellslinger+6"
 	tr = gol.SearchUrl().Title("spellslinger 6").Construct()
 	if tr != searchTitleURL {
 		t.Errorf("Incorrect URL construction of Search query")
 	}
 
+	var searchAuthorURL = "https://openlibrary.org/search.json?&author=Sarah+Penner"
 	tr = gol.SearchUrl().Author("Sarah Penner").Construct()
 	if tr != searchAuthorURL {
 		t.Errorf("Incorrect URL construction of Search query")
 	}
 
+	var searchSubjectURL = "https://openlibrary.org/search.json?&subject=abcd"
 	tr = gol.SearchUrl().Subject("abcd").Construct()
 	if tr != searchSubjectURL {
 		t.Errorf("Incorrect URL construction of Search query")
 	}
 
 	tr = gol.SearchUrl().Author("Richard Dawkins").Title("The Selfish Gene").Subject("evolution").Construct()
+	var searchMixedURL = "https://openlibrary.org/search.json?&title=The+Selfish+Gene&author=Richard+Dawkins&subject=evolution"
 	if tr != searchMixedURL {
 		t.Errorf("Incorrect URL construction of Search query")
 	}
 }
-*/
